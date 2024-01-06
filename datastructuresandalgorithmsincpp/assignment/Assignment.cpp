9dfe4e5130322cf3d2448b217a07b7a36d572e38
TedXpro <johannes.woldeyes@gmail.com>
2024-01-06 07:41:14 +0300

    create the Student and course class

--- datastructuresandalgorithmsincpp/assignment/Assignment.cpp
+++ datastructuresandalgorithmsincpp/assignment/Assignment.cpp
@@ -1,3 +1,88 @@
 #include <iostream>
 
 using namespace std;
+
+class Student
+{
+private:
+    int studentId;
+    string firstName;
+    string lastName;
+    int age;
+    char sex;
+
+public:
+    Student(int id){
+        studentId = id;
+    }
+    // ~Student();
+
+    int getId(){
+        return studentId;
+    }
+
+    string getFirstName(){
+        return firstName;
+    }
+    string getLastName(){
+        return lastName;
+    }
+
+    int getAge(){
+        return age;
+    }
+
+    char getSex(){
+        return sex;
+    }
+
+    void setFirstName(string fName){
+        firstName = fName;
+    }
+
+    void setLastName(string lName){
+        lastName = lName;
+    }
+
+    void setAge(int a){
+        age = a;
+    }
+
+    void setSex(char s){
+        sex = s;
+    }
+
+};
+
+class Course{
+private:
+    string courseNumber;
+    string courseTitle;
+    int creditHour;
+
+public:
+    Course(string courseNum){
+        courseNumber = courseNum;
+    }
+
+    string getCourseNumber(){
+        return courseNumber;
+    }
+
+    string getCourseTitle(){
+        return courseTitle;
+    }
+
+    int getCreditHour(){
+        return creditHour;
+    }
+
+    void setCourseTitle(string coTitle){
+        courseTitle = coTitle;
+    }
+
+    void setCreditHour(int creditHr){
+        creditHour = creditHr;
+    }
+};
+
---
+++ datastructuresandalgorithmsincpp/assignment/Student.h
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+
+#ifndef STUDENT_H
+#define STUDENT_H
+
+
+
+#endif
