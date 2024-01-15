6c4ee7e04dd1e613489b004ca6e9775f628f2e00
TedXpro <johannes.woldeyes@gmail.com>
2024-01-15 17:42:25 +0300

    convert infix to postfix using stack

---
+++ datastructuresandalgorithmsincpp/lab/infixToPostfix.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main()
+{
+    string infixExpression = "(a - b * c) / (d * e ^ f * g + h)"; // "a-(b / (c-d) * e + f) ^g"
+    string postfixExpression = "";
+    stack<char> st;
+
+    for (char ch : infixExpression)
+    {
+        if (ch == ' ')
+            continue;
+        if (ch >= 'a' && ch <= 'z')
+            postfixExpression += ch;
+        else if (ch == '+' || ch == '-')
+        {
+            while (!st.empty() && (st.top() != '('))
+            {
+                postfixExpression += st.top();
+                st.pop();
+            }
+            st.push(ch);
+        }
+        else if (ch == '*' || ch == '/')
+        {
+            while (!st.empty() && (st.top() == '*' || st.top() == '/' || st.top() == '^'))
+            {
+                postfixExpression += st.top();
+                st.pop();
+            }
+            st.push(ch);
+        }
+        else if (ch == ')')
+        {
+            while (st.top() != '(')
+            {
+                postfixExpression += st.top();
+                st.pop();
+            }
+            st.pop();
+        }
+        else
+            st.push(ch);
+    }
+
+    while (!st.empty())
+    {
+        postfixExpression += st.top();
+        st.pop();
+    }
+
+    cout << postfixExpression << endl;
+}
